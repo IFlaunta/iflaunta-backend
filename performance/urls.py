2f20940d6d074ddc534b5f07dc729cf5c1295c81
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-09-01 00:58:26 +0530

    Added views and urls for user model with authentication

--- performance/urls.py
+++ performance/urls.py
@@ -3,5 +3,8 @@ from performance import views
 
 app_name = 'performance'
 urlpatterns = [
+    path("add_user/", views.addUser.as_view(), name="add_user"),
+    path("get_user_from_email/<email>/", views.getUserFromEmail.as_view(), name="get_user_from_email"),
+    path("get_user_from_user_id/<int:user_id>/", views.getUserFromUserId.as_view(), name="get_user_from_user_id")
 
 ]
--- performance/views.py
+++ performance/views.py
@@ -2,5 +2,57 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from performance.models import User
+from performance.serializers import UserSerializer
+
+def remove_password(data):
+    if(isinstance(data, dict)):
+        data.pop("password", "Doesn't contain password")
+    return
+
+class addUser(APIView):
+
+    def post(self, request):
+        data = request.data
+        serializer = UserSerializer(data=data)
+        if(serializer.is_valid()):
+            user = serializer.save(password=make_password(data["password"]))
+            print(type(user))
+            data = serializer.data
+            remove_password(data)    # Removing hashed password
+            return Response(data=data, status=status.HTTP_201_CREATED)
+        return Response(data={"error": "Please enter valid data :("}, status=status.HTTP_400_BAD_REQUEST)
+    
+class getUserFromEmail(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, email):
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response(data={"error": "User does not exist :("}, status=status.HTTP_404_NOT_FOUND)
+        
+        data = UserSerializer(user).data
+        remove_password(data)    # Removing hashed password        
+        return Response(data=data, status=status.HTTP_200_OK)
+    
+class getUserFromUserId(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, user_id):
+        try:
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            return Response(data={"error": "User does not exist :("}, status=status.HTTP_404_NOT_FOUND)
+        
+        data = UserSerializer(user).data
+        remove_password(data)    # Removing hashed password
+        return Response(data=data, status=status.HTTP_200_OK)
+
+
+        
