84d288cf9eed5bb974887925d0cb51d4d1f33d87
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-09-01 00:15:20 +0530

    Added user model and serializer

--- performance/models.py
+++ performance/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+        return user
+
+class User(AbstractUser):
+    username = None
+    user_id = models.AutoField(primary_key=True, editable=False)
+    email = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    password = models.CharField(max_length=255)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+    
+    def get_short_name(self):
+        return self.first_name
+    
+    def __str__(self):
+        return self.email
+
+
