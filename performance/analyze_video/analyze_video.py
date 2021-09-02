64110e1e457a1297bf176e4d471adca7f86a2367
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-09-03 02:35:53 +0530

    Added method for getting audio from video

--- performance/analyze_video/analyze_video.py
+++ performance/analyze_video/analyze_video.py
@@ -10,4 +10,14 @@ class AnalyzeVideo:
     def __init__(self, videoLocation):
         self.videoLocation = videoLocation
 
+    def get_audio(self):
+        '''
+        For generating audio from the video in mp3 format
+        '''
+        # Keeping the audio file name same as the video file, 
+        # just changing the externsion to mp3
+        audioLocation = self.videoLocation[0:self.videoLocation.rindex(".")]+".mp3"
+        video_clip = mp.VideoFileClip(self.videoLocation)
+        video_clip.audio.write_audiofile(audioLocation)
+        self.audioLocation = audioLocation
     
