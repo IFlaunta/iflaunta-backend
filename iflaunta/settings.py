9b4b51aa552979381ba9f41685f44728b5bc98ad
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-09-03 14:27:30 +0530

    Added analyze_audio method for getting cofidence and transcript

--- iflaunta/settings.py
+++ iflaunta/settings.py
@@ -149,3 +149,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+IBM_URL = os.getenv("IBM_URL")
+IBM_API_KEY = os.getenv("IBM_API_KEY")
