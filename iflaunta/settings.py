2193af736496048f4aa20a39049b81e154d97a6e
Pratyush1606 <jaiswalpratyush2015@gmail.com>
2021-12-29 13:18:15 +0530

    Fixed db design

--- iflaunta/settings.py
+++ iflaunta/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (True if os.getenv("DJANGO_DEBUG")=="True" else False)
 
-ALLOWED_HOSTS = ['iflaunta.herokuapp.com']
+ALLOWED_HOSTS = ['iflaunta.herokuapp.com', '127.0.0.1']
 
 SESSION_COOKIE_SECURE = (True if os.getenv("DJANGO_SESSION_COOKIE_SECURE")=="True" else False)
 
